77f27bdf7fe29490c31bb39188d032e567d1cb39
Luciano da Silva <luciano.silva@live.com.pt>
2013-04-24 12:09:44 +0100

    Component and Messaging Abstraction #10

--- source/engine/include/ballistic.engine.icomponent.h
+++ source/engine/include/ballistic.engine.icomponent.h
@@ -2,16 +2,31 @@
 #ifndef _ballistic_engine_icomponent_h_
 #define _ballistic_engine_icomponent_h_
 
+#include "ballistic.engine.message.h"
+
 namespace ballistic {
 	namespace engine {
 	
 		class icomponent {
 		public:
-			
-			virtual void on_message () = 0;
+
+			virtual void notify ( const ballistic::engine::message & message ) = 0;
 			
 		};
-		
+
+		template < void (*message_handle)( const ballistic::engine::message & ) >
+		class func_component : public icomponent {
+		public:
+
+			virtual inline void notify ( const ballistic::engine::message & message );
+
+		};
+
+		template < void (*message_handle)( const ballistic::engine::message & ) >
+		void func_component < message_handle >::notify ( const ballistic::engine::message & message ) {
+			message_handle (message);
+		}
+
 	}
 }
 
--- source/engine/include/ballistic.engine.message.h
+++ source/engine/include/ballistic.engine.message.h
@@ -1,12 +1,63 @@
 #ifndef _ballistic_engine_message_h_
 #define _ballistic_engine_message_h_
 
+#include "ballistic.var.h"
+
+#include <functional>
+#include <map>
+#include <string>
+
+using namespace std;
+
 namespace ballistic {
 	namespace engine {
-		
-		struct message {
+
+		class message {
+		public:
+
+			typedef size_t message_id_t;
+
+		private:
 			
+			map < size_t, var >		_attributes;
+			message_id_t			_message_id;
+
+		public:
+
+			inline message ( message_id_t & message_id );
+			inline message ( const message & orig );
+
+			inline message_id_t get_id () const;
+
+			inline bool has_attribute ( size_t attribute_key ) const;
+			inline bool has_attribute ( const string & key ) const;
+
+			inline var & operator [] ( size_t attribute_key );
+			inline var & operator [] ( const string & key );
+
 		};
+
+		message::message ( message_id_t & message_id ) : _message_id (message_id) {}
+
+		message::message ( const message & orig ) : _message_id ( orig._message_id ), _attributes (orig._attributes) {}
+
+		inline message::message_id_t message::get_id () const { return _message_id; }
+
+		bool message::has_attribute ( size_t attribute_key ) const {
+			return _attributes.find (attribute_key) != _attributes.end ();
+		}
+
+		bool message::has_attribute ( const string & key ) const {
+			return has_attribute ( hash < string >() (key) ) ;
+		}
+
+		var & message::operator [] ( size_t attribute_key ) {
+			return _attributes [attribute_key];
+		}
+
+		var & message::operator [] ( const string & key ) {
+			return operator [] ( hash < string > ()(key) );
+		}
 		
 	}
 }
