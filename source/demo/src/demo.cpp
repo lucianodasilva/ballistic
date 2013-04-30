c0357402e3202297ba24965d5b9e731454750149
Luciano da Silva <luciano.silva@live.com.pt>
2013-04-30 09:29:42 +0100

    Minor changes to build glow

--- source/demo/src/demo.cpp
+++ source/demo/src/demo.cpp
@@ -1,23 +1,65 @@
-//#include <ballistic.h>
-#include <GL/glfw.h>
+#include <GL/glfw3.h>
 
-int main ( int argc, char ** argv ) {
-    /* Initialize the library */
+#include <stdlib.h>
+#include <stdio.h>
+
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+	
+    glfwSetErrorCallback(error_callback);
+	
     if (!glfwInit())
-        return -1;
+        exit(EXIT_FAILURE);
 	
-    /* Create a windowed mode window and its OpenGL context */
-    if (!glfwOpenWindow(640, 480, 8, 8, 8, 0, 24, 0, GLFW_WINDOW))
-        return -1;
-	
-    /* Loop until the user closes the window */
-    while (glfwGetWindowParam(GLFW_OPENED))
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
     {
-        /* Render here */
-		
-        /* Swap front and back buffers and process events */
-        glfwSwapBuffers();
+        glfwTerminate();
+        exit(EXIT_FAILURE);
     }
 	
-    return 0;
+    glfwMakeContextCurrent(window);
+	
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+		
+        glfwGetWindowSize(window, &width, &height);
+        ratio = width / (float) height;
+		
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+		
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        glMatrixMode(GL_MODELVIEW);
+		
+        glLoadIdentity();
+        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+		
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+		
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+	
+    glfwDestroyWindow(window);
+	
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
