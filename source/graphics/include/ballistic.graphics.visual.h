ce49f7fe33426f7f7aac62b40a69782eb8761d81
Luciano da Silva <luciano.silva@live.com.pt>
2013-09-16 18:50:02 +0100

    rename component to handle with less fuss

---
+++ source/graphics/include/ballistic.graphics.visual.h
@@ -0,0 +1,41 @@
+#ifndef _ballistic_graphics_visual_h_
+#define	_ballistic_graphics_visual_h_
+
+#include <ballistic.base.h>
+#include <vector>
+
+#include "ballistic.graphics.idevice.h"
+#include "ballistic.graphics.camera.h"
+
+namespace ballistic {
+	namespace graphics {
+
+		class graphics_system;
+
+		class visual : public ballistic::component {
+		private:
+
+			mat4				_transform;
+
+			imaterial *			_material;
+			imesh *				_mesh;
+
+			graphics_system *	_system;
+			game *				_game;
+
+		public:
+
+			static const id_t component_id;
+
+			visual ();
+
+			virtual void notify ( ballistic::message & message );
+
+			virtual void setup (vector < ballistic::property > & parameters);
+
+		};
+				  
+	}
+}
+
+#endif
---
+++ source/graphics/src/ballistic.graphics.visual.cpp
@@ -0,0 +1,79 @@
+#include "ballistic.graphics.visual.h"
+#include "ballistic.graphics.common_id.h"
+#include "ballistic.graphics.graphics_system.h"
+
+#include "ballistic.graphics.imaterial.h"
+#include "ballistic.graphics.imesh.h"
+
+namespace ballistic {
+	namespace graphics {
+
+		const id_t visual::component_id = ballistic::id::graphics::visual;
+
+		void visual::setup (vector < ballistic::property > & parameters)
+		{
+		}
+
+		visual::visual ()
+			:
+			_material (nullptr),
+			_mesh (nullptr),
+			_system (nullptr),
+			_game (nullptr)
+		{}
+
+		void visual::notify ( ballistic::message & message ) {
+
+			// if render... render :D 
+			if (message.get_id () == id::message_render) {
+
+				if (_system && _material && _material->get_effect () && _mesh)
+					_system->push_item (_material, _mesh, _transform);
+				debug_run ( else
+					debug_warn ("[ballistic::graphics::visual] Incomplete visual component. Will not render!");
+				);
+
+				return;
+			}
+
+			// handle property changes
+			if (message.get_id () == ballistic::id::message_property_changed && 
+				message.get_sender () == get_entity ()
+			){
+				id_t property_id = message [ballistic::id::id].as <id_t> ();
+
+				if (property_id == id::transform)
+					_transform = message [ballistic::id::value].as <mat4> ();
+
+				if (property_id == id::material) {
+					id_t material_res_id = message [ballistic::id::value].as < id_t > ();
+
+					_material = _game->get_resource < imaterial > (material_res_id);
+				}
+
+				if (property_id == id::mesh) {
+					id_t mesh_res_id = message [ballistic::id::value].as < id_t > ();
+					_mesh = _game->get_resource < imesh > (mesh_res_id);
+				}
+
+				return;
+			}
+
+			// if initialize load game and entity properties
+			if (message.get_id () == ballistic::id::message_initialize) {
+
+				entity * ent = get_entity ();
+
+				_game = ent->get_game ();
+
+				_system = dynamic_cast <graphics_system *> (_game->find_system (ballistic::id::graphics::system));
+
+				_material	= _game->get_resource < imaterial > (ent->get_property (ballistic::id::material).as < id_t > ());
+				_mesh		= _game->get_resource < imesh > (ent->get_property (ballistic::id::mesh).as < id_t > ());
+				_transform	= ent->get_property (ballistic::id::transform).as < mat4 > ();
+			}
+
+		}
+
+	}
+}
